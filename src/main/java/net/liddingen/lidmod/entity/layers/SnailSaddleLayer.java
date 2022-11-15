282d6c20fc4f4f84af3866c38c06779b9a812016
Liddingen <leif.kuhnert@gmail.com>
2022-11-15 18:30:46 +0100

    paar sachen gemacht

--- src/main/java/net/liddingen/lidmod/entity/layers/SnailSaddleLayer.java
+++ src/main/java/net/liddingen/lidmod/entity/layers/SnailSaddleLayer.java
@@ -2,14 +2,24 @@ package net.liddingen.lidmod.entity.layers;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.liddingen.lidmod.LidMod;
+import net.liddingen.lidmod.entity.custom.SnailEntity;
+import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
-import software.bernie.geckolib3.GeckoLib;
+import net.minecraft.world.entity.Saddleable;
+import net.minecraft.world.item.HorseArmorItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.SaddleItem;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import software.bernie.geckolib3.renderers.geo.GeoLayerRenderer;
 import software.bernie.geckolib3.renderers.geo.IGeoRenderer;
+
+@OnlyIn(Dist.CLIENT)
 
 public class SnailSaddleLayer extends GeoLayerRenderer {
     // A resource location for the texture of the layer. This will be applied onto pre-existing cubes on the model
@@ -23,13 +33,17 @@ public class SnailSaddleLayer extends GeoLayerRenderer {
 
     @Override
     public void render(PoseStack matrixStackIn, MultiBufferSource bufferIn, int packedLightIn, Entity entityLivingBaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
-        RenderType cameo =  RenderType.armorCutoutNoCull(LAYER);
+
+        RenderType cameo = RenderType.armorCutoutNoCull(LAYER);
         matrixStackIn.pushPose();
         //Move or scale the model as you see fit
         matrixStackIn.scale(1.0f, 1.0f, 1.0f);
         matrixStackIn.translate(0.0d, 0.0d, 0.0d);
-        this.getRenderer().render(this.getEntityModel().getModel(MODEL), entityLivingBaseIn, partialTicks, cameo, matrixStackIn, bufferIn,
-                bufferIn.getBuffer(cameo), packedLightIn, OverlayTexture.NO_OVERLAY, 1f, 1f, 1f, 1f);
-        matrixStackIn.popPose();
+       /* ItemStack itemstack = new ItemStack(Items.SADDLE);
+        if (itemstack.getItem() instanceof SaddleItem) {
+            SaddleItem SaddleItem = (SaddleItem) itemstack.getItem(); */ //Id saddled
+            this.getRenderer().render(this.getEntityModel().getModel(MODEL), entityLivingBaseIn, partialTicks, cameo, matrixStackIn, bufferIn,
+                    bufferIn.getBuffer(cameo), packedLightIn, OverlayTexture.NO_OVERLAY, 1f, 1f, 1f, 1f);
+            matrixStackIn.popPose();
+        }
     }
-}
